6d88b31c06bc3885006f4dd5ad4f09fd4070257f
arieh <arieh.glazer@gmail.com>
2011-02-21 11:33:28 +0200

    added a post partial

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ActiveRecord::Base
     :foreign_key => "base"
   belongs_to :base_post, :class_name => "Post", :foreign_key=>'base'
   
-  has_one :parent, :class_name =>"Post", :foreign_key =>"parent_id"
+  belongs_to :parent, :class_name =>"Post", :foreign_key =>"parent_id"
 
   belongs_to :user
   
