d06378e3539999673e7140ac8ec19bd7f1b49238
arieh <arieh.glazer@gmail.com>
2011-02-21 15:25:32 +0200

    added registration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
-  devise :database_authenticatable, :invitable,
+  devise :database_authenticatable, :invitable, :registerable
          :recoverable, :rememberable, :trackable, :validatable
   has_many :posts
 
