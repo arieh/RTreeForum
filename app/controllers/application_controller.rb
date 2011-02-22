4fbc65cb94229316ac0c03555be9cbfb252e7c24
arieh <arieh.glazer@gmail.com>
2011-02-22 10:37:59 +0200

    another attemp at fixing mailer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   before_filter :authenticate_user!, :except => [:index]
   protect_from_forgery
+
+  before_filter :mailer_set_url_options
+ 
+  def mailer_set_url_options
+    ActionMailer::Base.default_url_options[:host] = request.host_with_port
+  end
 end
